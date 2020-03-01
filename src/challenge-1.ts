f1c9b402b4e9134f174df3763e1e4d74384c4854
Sergio Lopez <sloppy@sloppylopez.com>
2020-03-01 14:49:29 +0100

    Solved by reducing and calculating difference between current and previous values

--- src/challenge-1.ts
+++ src/challenge-1.ts
@@ -4,6 +4,7 @@
  * Find the closest number to num from the listOfNumbers. Assume the input array only contains unique values
  */
 export const getClosestValue = (num: number, listOfNumbers: number[]): number => {
-    // Write code here:
-    return 0;
+    return listOfNumbers.reduce(function(prev: number, curr: number) {
+        return (Math.abs(curr - num) < Math.abs(prev - num) ? curr : prev);
+    });
 };
